da9aeedd6f47b203a22bd7cb5aef0d687fecf1f7
Pierre Maurier <pierre.maurier@gadz.org>
2021-11-22 22:05:58 +0100

    Monkey patch gevent if gevent is found in order to be able to use uwsgi

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 """Runs the Fox in the Forest Flask server"""
+try:
+    from gevent import monkey
+except ModuleNotFoundError:
+    print("gevent module not found. Must be using Flask dev server.")
+    pass
+else:
+    monkey.patch_all()
+
 from foxy import app, socketio
 
 if __name__ ==  '__main__':
